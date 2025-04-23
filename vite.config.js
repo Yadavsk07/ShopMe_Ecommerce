04ae5c56c06bc112cf593ba597f5aca935ca386a
Yadavsk07 <kumar.sumit07092003@gmail.com>
2025-04-23 17:55:28 +0530

    Simplify configuration and fix paths

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ShopMe_Ecommerce/',
+  base: '',
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
+    outDir: 'dist'
   },
 })
