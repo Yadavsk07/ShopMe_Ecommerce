a133b5ca116c764775d07f61e167b36895866302
Yadavsk07 <kumar.sumit07092003@gmail.com>
2025-04-23 12:55:53 +0530

    Simplify configuration and fix asset loading

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/ShopMe_Ecommerce/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  },
 })
